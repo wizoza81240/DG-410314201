16121ce0e46b5fd89051c329f2d873320bf4ffae
wizoza81240 <wizoza81240@gmail.com>
2018-04-20 11:54:02 +0800

    第五題
    添加砲塔  設定旋轉程式

---
+++ Assets/movegun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movegun : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //transform.Translate(x,0,z);
+        
+
+        rb.AddForce(new Vector3(x,0,z))*speed);
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+    }
+}
